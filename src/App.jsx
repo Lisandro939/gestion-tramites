ed312e2b97cbe977012b7c868c25f4f16fd78615
lisandrofernandez2705 <lisandrofernandez2705@gmail.com>
2024-06-23 00:36:41 +0000

    FEAT: Add route "/completar-documentacion"

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RequestProcedure from './pages/request-process/RequestProcedure';
+import RequestProcedure from './pages/request-procedure/RequestProcedure';
 import Home from './pages/Home';
 import Layout from './components/Layout';
+import CompleteDocumentation from './pages/complete-documentation/CompleteDocumentation';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/solicitar-tramite" element={<RequestProcedure />} />
+          <Route path="/completar-documentación" element={<CompleteDocumentation />} />
         </Route>
       </Routes>
     </Router>
